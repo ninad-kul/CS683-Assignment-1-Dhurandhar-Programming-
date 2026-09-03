d70ee204211ab26685ee50b7b28f4b85a6f39e69
Ninad Narendra Kulkarni <139106764+ninad-kul@users.noreply.github.com>
2026-09-03 19:32:21 +0530

    Update conv_reorder.cpp
    Tried some small changes here and there

--- task1/conv_reorder.cpp
+++ task1/conv_reorder.cpp
@@ -2,26 +2,27 @@
 // Hint: loops from outermost to innermost -> ky, kx, oy, ox.
 
 #include "convolution.h"
-#include <cstring> // for using std::memset
 
-void conv_reorder(const float* in, float* out, const float* ker,
-                  int H, int W, int K) {
-                    
-const int p = K / 2;
-    const int in_stride = W + 2 * p;
+void conv_reorder(const float* in, float* out, const float* ker, int H, int W, int K) {
+    int pad = K / 2;
+    int stride = W + 2 * pad;
 
-    std::memset(out, 0, H * W * sizeof(float));
-
-    for (int ky = 0; ky < K; ++ky) {
-        for (int kx = 0; kx < K; ++kx) {
+    for (int y = 0; y < H; ++y) {
+        float* out_row = &out[y * W];
+        
+        for (int x = 0; x < W; ++x) {
+            float sum = 0.0f;
             
-            float w = ker[ky * K + kx];
-
-            for (int oy = 0; oy < H; ++oy) {
-                for (int ox = 0; ox < W; ++ox) {
-                    out[oy * W + ox] += w * in[(oy + ky) * in_stride + (ox + kx)];
+            // Reorder
+            for (int ky = 0; ky < K; ++ky) {
+                const float* in_row = &in[(y + ky) * stride + x];
+                const float* ker_row = &ker[ky * K];
+                
+                for (int kx = 0; kx < K; ++kx) {
+                    sum += in_row[kx] * ker_row[kx];
                 }
             }
+            out_row[x] = sum;
         }
     }
 }
