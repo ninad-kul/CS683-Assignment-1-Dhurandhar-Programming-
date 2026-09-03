83cc2f8d4a5bbe2d8b6ec6fe1be481e55d94fe80
Ninad Narendra Kulkarni <139106764+ninad-kul@users.noreply.github.com>
2026-09-03 19:56:14 +0530

    Update conv_tile.cpp
    Small changes. very little performance increase

--- task1/conv_tile.cpp
+++ task1/conv_tile.cpp
@@ -2,32 +2,37 @@
 
 #include "convolution.h"
 
-void conv_tile(const float* in, float* out, const float* ker,
-               int H, int W, int K) {
-
-   const int p = K / 2;
+void conv_tile(const float* in, float* out, const float* ker, int H, int W, int K) {
+    const int p = K / 2;
     const int in_stride = W + 2 * p;
 
+    // 32x256 tile uses ~34KB memory, fit in L1/L2 
+    // reduces outer loop iterations
     constexpr int TILE_H = 32;
-    constexpr int TILE_W = 32;
+    constexpr int TILE_W = 256;
 
     for (int by = 0; by < H; by += TILE_H) {
         for (int bx = 0; bx < W; bx += TILE_W) {
 
-            // processing output pixel within this tile completely
+            // Process output pixels within the tile
             for (int oy = by; oy < by + TILE_H; ++oy) {
+                float* out_row = &out[oy * W];
+                
                 for (int ox = bx; ox < bx + TILE_W; ++ox) {
+                    float acc = 0.0f;
                     
-                    float acc = 0.0f; // register accumulation (for faster operation)
+                    // row pointer math outside the loop
                     for (int ky = 0; ky < K; ++ky) {
+                        const float* in_row = &in[(oy + ky) * in_stride];
+                        const float* ker_row = &ker[ky * K];
+                        
                         for (int kx = 0; kx < K; ++kx) {
-                            acc += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
+                            acc += in_row[ox + kx] * ker_row[kx];
                         }
                     }
-                    out[oy * W + ox] = acc; // single store per pixel
+                    out_row[ox] = acc;
                 }
             }
-
         }
     }
 }
