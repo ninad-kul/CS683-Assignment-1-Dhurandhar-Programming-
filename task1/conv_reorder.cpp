08fd992023a14ad56763ed1631c484d691d990c9
Ninad Narendra Kulkarni <139106764+ninad-kul@users.noreply.github.com>
2026-09-07 06:46:35 +0530

    Update conv_reorder.cpp

--- task1/conv_reorder.cpp
+++ task1/conv_reorder.cpp
@@ -1,29 +1,38 @@
 // conv_reorder.cpp  STAGE 1: LOOP REORDERING
 // Hint: loops from outermost to innermost -> ky, kx, oy, ox.
 
+
 #include "convolution.h"
 
 void conv_reorder(const float* in, float* out, const float* ker, int H, int W, int K) {
     int pad = K / 2;
     int stride = W + 2 * pad;
 
+  
     for (int y = 0; y < H; ++y) {
         float* out_row = &out[y * W];
-        
         for (int x = 0; x < W; ++x) {
-            float sum = 0.0f;
+            out_row[x] = 0.0f;
+        }
+    }
+
+ 
+    for (int ky = 0; ky < K; ++ky) {
+        for (int kx = 0; kx < K; ++kx) {
             
-            // Reorder
-            for (int ky = 0; ky < K; ++ky) {
-                const float* in_row = &in[(y + ky) * stride + x];
-                const float* ker_row = &ker[ky * K];
-                
-                for (int kx = 0; kx < K; ++kx) {
-                    sum += in_row[kx] * ker_row[kx];
+  
+            float k_val = ker[ky * K + kx];
+
+            for (int y = 0; y < H; ++y) {
+             
+                float* out_row = &out[y * W];
+                const float* in_row = &in[(y + ky) * stride + kx];
+
+           
+                for (int x = 0; x < W; ++x) {
+                    out_row[x] += in_row[x] * k_val;
                 }
             }
-            out_row[x] = sum;
         }
     }
 }
-
